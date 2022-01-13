4c3e7a902d307e13b0a10f09a054adc6f0692251
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-01-13 18:42:21 +0100

    Added exercise n 5

--- js/script5.js
+++ js/script5.js
@@ -3,3 +3,31 @@ Crea due array che hanno un numero di elementi diversi.
 Aggiungi elementi casuali all’array che ha meno elementi,
 fino a quando ne avrà tanti quanti l’altro
 */
+
+const list1 = [12, 24, 8, 90, 6, 18, 23];
+const list2 = [45, 60, 2];
+
+console.log(`List1`, list1);
+console.log(`List2`, list2);
+
+let isAllowed = false;
+let i = 0;
+
+while (list1.length !== list2.length) {
+   let number = Math.floor(Math.random() * 100) + 1;
+
+   if(list1.length > list2.length){
+      isAllowed = true;
+      list2.push(number);
+   } else if (list1.length < list2.length){
+      isAllowed = true;
+      list1.push(number);
+   } else {
+      console.log(`List1`, list1);
+      console.log(`List2`, list2);
+   }
+   i++;
+};
+
+console.log(`List1`, list1);
+console.log(`List2`, list2);
