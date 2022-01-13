ec30dd5c187c7ec321c4262a24cfb5aef377a204
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-01-13 18:47:48 +0100

    Fixed exercise 5

--- js/script5.js
+++ js/script5.js
@@ -10,23 +10,21 @@ const list2 = [45, 60, 2];
 console.log(`List1`, list1);
 console.log(`List2`, list2);
 
-let isAllowed = false;
-let i = 0;
 
 while (list1.length !== list2.length) {
    let number = Math.floor(Math.random() * 100) + 1;
 
    if(list1.length > list2.length){
-      isAllowed = true;
+      
       list2.push(number);
    } else if (list1.length < list2.length){
-      isAllowed = true;
+      
       list1.push(number);
    } else {
       console.log(`List1`, list1);
       console.log(`List2`, list2);
    }
-   i++;
+   
 };
 
 console.log(`List1`, list1);
