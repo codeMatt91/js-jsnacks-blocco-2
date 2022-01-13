f6d77341ca5568b76f6ceac6e8fffad3755fe3f7
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-01-13 16:50:43 +0100

    created script 3

--- js/script3.js
+++ js/script3.js
@@ -2,4 +2,13 @@
 Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
 lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
 
+1- creo una lista di nomi 
+2- creo un a lista di cognomi
+3- genero una falsa lista di coppie nome-cognome
 */
+
+
+const nomi = ['matteo', 'giovanni', 'marco', 'luca', 'anna'];
+const cognomi = ['bigotti', 'rossi', 'verdi', 'neri'];
+
+
